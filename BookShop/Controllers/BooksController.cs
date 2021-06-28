5143f73767c1d213acd56fc670ae426f2f8f5e3d
KenanAL <kenan.rawashdah@gmail.com>
2021-06-28 03:15:33 +0300

    initail commit

---
+++ BookShop/Controllers/BooksController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class BooksController : Controller
+    {
+        public static List<Book> listBooks = new List<Book>();
+
+        // GET: BooksController
+        public ActionResult Index()
+        {
+            return View(listBooks);
+        }
+
+        //search  Book
+        public ActionResult Search(string searchText)
+
+        {
+            searchText = searchText.ToLower();
+
+            List<Book> searchResults = listBooks.FindAll(b =>
+                   b.Title.ToLower().Contains(searchText) || b.Description.ToLower().Contains(searchText)
+                   || b.Authors.ToLower().Contains(searchText));
+            return View(searchResults);
+        }
+
+        // GET: BooksController/Details/5
+        public ActionResult Details(int id)
+        {
+            Book model = listBooks.FirstOrDefault(b => b.BookId == id);
+            if (model is null)
+            {
+                return View(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: BooksController/Create
+        public ActionResult Create()
+        {
+            return View(new Book());
+        }
+
+        // POST: BooksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book model)
+        {
+            try
+            {
+                DateTimeOffset now = (DateTimeOffset)DateTime.UtcNow;
+
+                if (ModelState.IsValid) {
+                    model.BookId = now.ToUnixTimeSeconds();
+                    listBooks.Add(model);
+                return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Book model = listBooks.FirstOrDefault(b => b.BookId == id);
+               if(model is null)
+            {
+                return View(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // POST: BooksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Book model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    listBooks.Remove(listBooks.FirstOrDefault(b => b.BookId == id));
+                    listBooks.Add(model);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Book model = listBooks.FirstOrDefault(b => b.BookId == id);
+            if (model is null)
+            {
+                return View(nameof(Index));
+            }
+            return View(model);
+
+        }
+         
+        public ActionResult DeleteBook(int id)
+        {
+            try
+            {
+                listBooks.Remove(listBooks.FirstOrDefault(b => b.BookId == id));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: BooksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Book Model)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BookShop/Models/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class Book
+    {
+        //Id unique for each book
+        [Key()]
+        public long BookId { get; set; }
+
+        //Title of the book(Required)
+        [MaxLength(20, ErrorMessage = "It should not exceed 20 characters!")]
+        [MinLength(5, ErrorMessage = "It must be more than 5 characters!")]
+        [Required(ErrorMessage = "Title is Required!")]
+        public string Title { get; set; }
+
+        //Description of the book (length 5 – 100 (required) )
+        [Required(ErrorMessage = "Description is Required")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Description should have 5 up to 100 char!")]
+        public string Description { get; set; }
+
+        //Price of the book (minimum 1.50 $  - 1,500.00 $ (required) )
+        [Required(ErrorMessage = "Price is Required")]
+        [Range(1.5, 1500.0, ErrorMessage = "Fees should be between 1.5$ and 1500.0$!")]
+        public double Price { get; set; }
+
+        //Author of the book (length 5 – 30 (required) )
+        [Required(ErrorMessage = "Authors is Required")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "It should between 5 to 30 characters!")]
+        public string Authors { get; set; }
+
+        //Published date (default Date-time-Now)
+        [Display(Name = "Published Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+
+        //Img of type URL
+        [Display(Name = "Image Url(optional)", Prompt ="Enter url iamge")]
+        [DataType(DataType.Url, ErrorMessage = "Please enter an image 'url'")]
+        public string ImageUrl { get; set; }
+
+        //#No of copies (minimum 1 – 100 (required) ) 
+        [Range(1, 100, ErrorMessage = "It should between 1 to 100!")]
+        public int Count { get; set; }
+
+        //Category of the book
+        public BookCategory Category { get; set; } 
+
+    }
+}
